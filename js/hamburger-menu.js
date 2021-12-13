96d3580a958259b8100b6ece212901a331427081
Dines <dinesvlarsen@gmail.com>
2021-12-13 17:36:39 +0100

    removed whitespace

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -4,10 +4,7 @@ const closeHamburgerMenu = document.querySelector('.nav__close-hamburger')
 const navMenu = document.querySelector('.nav__menu')
 const main = document.querySelector('main');
 
-
-
 header.addEventListener('click', navOnClick)
-
 function navOnClick(event) {
 	if (event.target.classList.contains('nav__hamburger') || event.target.classList.contains('nav__close-hamburger')) {
 		hamburgerMenu.classList.toggle('nav--hidden');
